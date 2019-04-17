55155982b1b8cb9fb1b57c6eee4bf03f8f046edd
Venitryx <47960552+Venitryx@users.noreply.github.com>
2019-04-17 16:48:05 -0400

    Removed unused assets and organized them

---
+++ 4_Argon_Assault/Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame 
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
