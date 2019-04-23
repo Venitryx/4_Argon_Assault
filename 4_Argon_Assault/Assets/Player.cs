cd1ed41a1bfee3abda9181c23c780c04f778a121
Venitryx <47960552+Venitryx@users.noreply.github.com>
2019-04-22 20:18:42 -0400

    Updated Player Script
    Added pitch, yaw, and roll in relations to the camera so the aircraft doesn't look like it's always pointing towards the center

---
+++ 4_Argon_Assault/Assets/Player.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("in meters/second")] [SerializeField] float xSpeed = 4f;
+    [SerializeField] float xMinPos = -5f;
+    [SerializeField] float xMaxPos = 5f;
+    [SerializeField] float yMinPos = -3f;
+    [SerializeField] float yMaxPos = 5f;
+
+    [SerializeField] float positionPitchFactor = -1.5f;
+    [SerializeField] float controlPitchFactor = -5f;
+
+    [SerializeField] float positionYawFactor = 0.75f;
+
+    [SerializeField] float controlRollFactor = -30f;
+
+    float xThrow, yThrow;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessLocation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        //x = pitch, y = yaw, z = roll
+        float pitch, yaw, roll;
+        pitch = transform.localPosition.y * positionPitchFactor + yThrow * controlPitchFactor;
+        yaw = transform.localPosition.x * positionYawFactor;
+        roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessLocation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float yOffset = yThrow * xSpeed * Time.deltaTime;
+
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+
+        float xPos = Mathf.Clamp(rawNewXPos, xMinPos, xMaxPos);
+        float yPos = Mathf.Clamp(rawNewYPos, yMinPos, yMaxPos);
+
+        transform.localPosition = new Vector3(xPos, yPos, transform.localPosition.z);
+    }
+}
